06c688565dcc079183d819b5bb1aa743c5912f63
Faraja17 <farajakthompson@gmail.com>
2022-05-23 12:09:29 -0400

    Create rps-game.js
    Wrote outline of the entire design plan in comments

---
+++ rps-game.js
@@ -0,0 +1,36 @@
+// variables to keep track of the number of wins, losses, and ties
+
+
+// outer main game loop
+
+
+// inner player input loop
+
+
+// quit program if player selects q
+
+
+// break out of player loop if r, p, or s, is selected, otherwise remind to select one.
+
+
+// display what the player chose
+
+
+
+// display what the computer chose
+
+
+
+// compare player vs. computer, then display and record the total wins/losses/ties
+
+
+
+// tie
+
+
+
+// win
+
+
+
+// loss
